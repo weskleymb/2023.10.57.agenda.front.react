f91413a867b9b092bcaecfa07e1427af85a5664f
Weskley Bezerra <weskleymb@hotmail.com>
2024-03-25 15:44:06 -0300

    initial commit

---
+++ src/components/contato/contato-list.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// declaração de tipos
+interface Fone {
+    id: number;
+    numero: string;
+}
+
+interface Contato {
+  id: number;
+  nome: string;
+  fone: Fone;
+}
+
+const ContatoList: React.FC = () => {
+  const [contatos, setContatos] = useState<Contato[]>([]);
+
+  useEffect(() => {
+    const fetchContatos = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/contatos');
+        console.log(response.data);
+        setContatos(response.data);
+      } catch (error) {
+        console.error('Houve um erro ao buscar os contatos:', error);
+      }
+    };
+    fetchContatos();
+  }, []);
+
+  return (
+    <div>
+      <h2>Lista de Contatos</h2>
+      <ul>
+        {contatos.map(contato => (
+          <li key={contato.id}>
+            {contato.nome} - {contato.fone.numero}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ContatoList;
